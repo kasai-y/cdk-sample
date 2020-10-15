4b53cb3e627a9aeeb941f9ae35c391b5663da76e
KASAI Yoshimitsu <kasai@photocreate.co.jp>
2020-10-15 09:23:53 +0900

    add s3 Bucket w/LifecycleRules

--- lib/cdk-sample-stack.ts
+++ lib/cdk-sample-stack.ts
@@ -2,6 +2,8 @@ import * as sns from '@aws-cdk/aws-sns';
 import * as subs from '@aws-cdk/aws-sns-subscriptions';
 import * as sqs from '@aws-cdk/aws-sqs';
 import * as cdk from '@aws-cdk/core';
+import * as s3 from '@aws-cdk/aws-s3';
+import {Expiration} from "@aws-cdk/core";
 
 export class CdkSampleStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
@@ -14,5 +16,14 @@ export class CdkSampleStack extends cdk.Stack {
     const topic = new sns.Topic(this, 'CdkSampleTopic');
 
     topic.addSubscription(new subs.SqsSubscription(queue));
+
+    const bucket = new s3.Bucket(this, 'CdkSampleBucket',{
+      bucketName : "test-kasai-2020-10-15",
+      lifecycleRules : [
+        {
+          expiration : cdk.Duration.days(1)
+        }
+      ]
+    })
   }
 }
